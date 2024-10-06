83b3b97ebde65f9129ee974757117ae74f07df4d
wissenschaftlerin <72699045+wissenschaftlerin@users.noreply.github.com>
2024-10-06 19:21:02 +0300

    Takım sayfası resim ekleme

--- src/components/TeamPage.js
+++ src/components/TeamPage.js
@@ -1,32 +1,35 @@
 import React from 'react';
 import Navbar from './Navbar'; // Import the Navbar component
 import FeyzaImage from '../assets/feyza_yuvarlak.png'; // Import the image
+import NisaImage from '../assets/nisa tan foto.png'; // Import the image
+import SelenImage from '../assets/selen özdinç foto.png'; // Import the image
+import ElifImage from '../assets/elif kaman foto.png'; // Import the image
 
 const TeamPage = () => {
   const teamMembers = [
     {
       name: 'Zeynep Feyza Atabey',
-      role: 'Chief Science Officer',
+      role: 'Chief Executive Officer',
       bio: 'With over 15 years of experience in nanotechnology and bioengineering, Dr. Clara leads our scientific research. Her groundbreaking work in nanoparticle production for medical treatments has garnered international recognition.',
       image: FeyzaImage // Use imported image here
     },
     {
       name: 'Selen Özdinç',
       role: 'Chief Technology Officer',
-      bio: 'Aiden is a master of engineering solutions, focusing on scaling nanoparticle production processes for mass-market applications. His innovative approaches have accelerated production without compromising quality.',
-      image: 'path-to-image-of-Aiden' // Replace with appropriate image path
+      bio: 'Selen is a master of engineering solutions, focusing on scaling nanoparticle production processes for mass-market applications. His innovative approaches have accelerated production without compromising quality.',
+      image: SelenImage
     },
     {
       name: 'Elif Kaman',
       role: 'Chief Innovation Officer',
-      bio: 'Lena brings over a decade of experience in marketing for biotech startups. She’s passionate about raising awareness of rare diseases and driving community engagement for our mission.',
-      image: 'path-to-image-of-Lena' // Replace with appropriate image path
+      bio: 'Elif brings over a decade of experience in marketing for biotech startups. She’s passionate about raising awareness of rare diseases and driving community engagement for our mission.',
+      image: ElifImage
     },
     {
       name: 'Nisa Tan',
       role: 'Chief Operating Officer',
       bio: 'Nisa is a Molecular Biotechnology student at Türk-Alman University and a valued team member at nanoWISS. She is passionate about biotech innovations and is leading the operations team with her strong academic background and leadership skills.',
-      image: 'path-to-image-of-Nisa' // Replace with appropriate image path
+      image: NisaImage
     }
   ];
 
